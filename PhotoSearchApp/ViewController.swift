525c4f424765cf3858508d307ff5f4a61854faff
t-takemoto1 <193842913+t-takemoto1@users.noreply.github.com>
2026-08-29 14:33:41 +0900

    [clean] ファイル整理

---
+++ PhotoSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoSearchApp
+//
+//  Created by takemoto on 2026/08/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
